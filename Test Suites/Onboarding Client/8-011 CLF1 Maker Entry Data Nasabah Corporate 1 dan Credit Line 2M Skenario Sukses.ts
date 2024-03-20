a38374eff42acfeced4518317ad262d446cea06f
rasaarah <ra.sarahkamilah@gmail.com>
2024-03-20 12:05:06 +0700

    Add test suite 8-011

--- Test Suites/Onboarding Client/8-011 CLF1 Maker Entry Data Nasabah Corporate 1 dan Credit Line 2M Skenario Sukses.ts
+++ Test Suites/Onboarding Client/8-011 CLF1 Maker Entry Data Nasabah Corporate 1 dan Credit Line 2M Skenario Sukses.ts
@@ -11,4 +11,11 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>d622daea-f1d6-4c0a-b1e8-3596df07c8b4</testSuiteGuid>
+   <testCaseLink>
+      <guid>da14c33f-d66c-4343-b5ce-d4abb74fbd4b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Onboarding Client/8-011 CLF1 Maker Entry Data Nasabah Corporate 1 dan Credit Line 2M Skenario Sukses</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
